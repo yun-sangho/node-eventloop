34400c570a3d37a3e438ccb3bf223c4d205f9581
Austin <austin@protopie.io>
2020-08-09 16:26:35 +0900

    예시 추가

--- step1.js
+++ step1.js
@@ -1,17 +1,19 @@
-const https = require('http');
-const timeoutScheduled = Date.now()
+const https = require("http");
+const timeoutScheduled = Date.now();
 
 setTimeout(() => {
   const delay = Date.now() - timeoutScheduled;
   console.log(`[setTimeout] ${delay}ms have passed since I was scheduled`);
 }, 2000);
 
-https.get('http://localhost:8080', (resp) => {
+https.get("http://localhost:8080", (resp) => {
   const delay = Date.now() - timeoutScheduled;
   console.log(`[Http] ${delay}ms have passed since I was scheduled`);
-})
+});
 
 setImmediate(() => {
   const delay = Date.now() - timeoutScheduled;
   console.log(`[setImmediate] ${delay}ms have passed since I was scheduled`);
 });
+
+console.log("I will run at first!");
